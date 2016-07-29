7338b9aafca00316af51422796c80b11dc2ff364
Richard Littauer <richard.littauer@gmail.com>
2016-07-28 22:32:51 -0400

    Finished?

--- README.md
+++ README.md
@@ -1,5 +1,46 @@
-# csv-schwab-to-ynab
+# ynab-convert-csv
 
-> A basic CSV converter for Charles Schwab CSV files to YNAB compatible ones
+> A converter for Banking transaction CSV files to YNAB format
 
 WIP.
+
+This tool lets you transform csvs from various banks into a format expected by YNAB. Currently accepted Schwab and TDBank only.
+
+## Install
+
+    npm install -g ynab-convert-csv
+
+## Usage
+
+    ynab-convert-csv --input=examples/schwab.csv --output=schwab_CONVERTED.csv [--map=map.json]
+
+## Mapping files
+
+If you supply a mapping file, we'll try to map transactions to payees and categories. This matching is pretty simple, keys in a JSON file are matched with transaction descriptions with a "starts with" check. So, for example, a transaction with a description of `VDP-PAYPAL` will match transactions that start with this (e.g. `VDP-PAYPAL *SPOTIF` would match). Beneath these keys is an object with two keys: `payee` & `category`, which you can use to specify your payees and categories. Payees and categories here work the same way they do in YNAB, so transfers, for example, will have the same rules as YNAB for whether they need a category or not.
+
+Examples speak louder than words, here's a sample `map.json`:
+
+    {
+      "VDP-Amazon *Mktplc": {
+        "payee": "Amazon Marketplace",
+        "category": "Everyday Expenses: Amazon"
+      },
+      "VDA-": {
+        "payee": "Transfer: Cash",
+        "category": ""
+      },
+      "D/D BORD GAIS EIRE": {
+        "payee": "Bord Gais",
+        "category": "Monthly Bills: Gas and Electricity"
+      }
+    }
+
+If a map isn't found, the transformer falls back to using the description for both `payee` and `category`.
+
+## Contribute
+
+Please do! I basically used the code from [this module for AIB](https://github.com/jasonmadigan/aib-to-ynab) by [@jasonmadigan](https://github.com/jasonmadigan), to whom I am very grateful. Happy to edit this or pass it along to others who are interested. Open an issue.
+
+## License
+
+MIT
---
+++ index.js
@@ -0,0 +1,106 @@
+#!/usr/bin/env node
+
+var csv = require('fast-csv')
+var fs = require('fs')
+var _ = require('lodash')
+
+var argv = require('yargs')
+  .usage('Usage: ynab-convert-csv--input=Transaction_Export.csv --output=Transaction_Export_CONVERTED.csv [--map=map.json]')
+  .demand(['input'])
+  .describe('output', `Optional output file - if not specified, will output as ${['input']}_CONVERTED.csv`)
+  .describe('map', 'Optional Payee and Category mapping file')
+  .argv
+
+var map = (argv.map) ? JSON.parse(fs.readFileSync(argv.map, 'utf8')) : null
+
+if (!argv.output) {
+  argv.output = `${argv.input.split('.')[0]}_CONVERTED.csv`
+}
+
+if (map) {
+  console.log('Using map file.')
+}
+
+function _match (description) {
+  var key = _.findKey(map, function (value, key) {
+    return _.startsWith(description, key)
+  })
+
+  if (!key) {
+    console.log('No Match', description)
+    return null
+  }
+
+  return map[key]
+}
+
+function mapCategory (description) {
+  if (!map) return
+
+  var match = _match(description)
+  if (!match) return description
+
+  return match.category
+}
+
+function mapPayee (description) {
+  if (!map) return description
+
+  var match = _match(description)
+  if (!match) return description
+
+  return match.payee
+}
+
+// TODO Remove first line and 'posted transactions lines' from schwab
+// TODO Add General Expenses mapping if ATM is in memo
+
+if (argv.bank === 'schwab') {
+  csv
+    .fromPath(argv.input, {
+      headers: true,
+      trim: true
+    })
+    .transform((obj) => {
+      return {
+        'Date': obj['Date'],
+        'Payee': mapPayee(obj.Description),
+        'Category': mapCategory(obj.Description),
+        'Memo': obj['Type'],
+        'Outflow': obj['Withdrawal (-)'],
+        'Inflow': obj['Deposit (+)']
+      }
+    })
+    .pipe(csv.createWriteStream({
+      headers: true
+    }))
+    .pipe(fs.createWriteStream(argv.output, {
+      encoding: 'utf8'
+    })
+  )
+} else if (argv.bank === 'td') {
+  csv
+    .fromPath(argv.input, {
+      headers: false,
+      trim: true
+    })
+    .transform((obj) => {
+      return {
+        'Date': obj[0],
+        'Payee': mapPayee(obj[4]),
+        'Category': mapCategory(obj[4]),
+        'Memo': null,
+        'Outflow': obj[5],
+        'Inflow': obj[6]
+      }
+    })
+    .pipe(csv.createWriteStream({
+      headers: true
+    }))
+    .pipe(fs.createWriteStream(argv.output, {
+      encoding: 'utf8'
+    })
+  )
+} else {
+  console.log('Please specify --bank as `td` or `schwab`')
+}
